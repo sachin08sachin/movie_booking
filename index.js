b57b2034288eebf2651cb614ffd7762d32825eca
sachin08sachin <sgsachin359@gmail.com>
2023-02-24 12:00:38 +0530

    Save Data to LocalStorage

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
 // update total and count
 function updateSelectedcount(e){
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
     // console.log(selectedSeats)
+
+    // LocalStorage
+       // Copy selectec seats into arr
+       // Map through arr
+       // return a new arr indexes
+       const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+       
+       localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount =selectedSeats.length;
     
     count.innerText = selectedSeatsCount;
@@ -19,6 +35,7 @@ function updateSelectedcount(e){
 //movie click event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedcount();
 })
 
